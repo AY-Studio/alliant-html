e93ebd74fa72a52f889c423ca5e16f5d7c36f020
Robert Annett <robert@ay.studio>
2025-10-23 09:36:49 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -4,6 +4,21 @@ const swup = new window.Swup({
   animationSelector: '[class*="transition-"]'
 });
 
+// Navbar shrink on scroll
+const navbar = document.querySelector('.navbar');
+
+const updateNavbarState = () => {
+  if (!navbar) return;
+  if (window.scrollY > 20) {
+    navbar.classList.add('navbar-shrink');
+  } else {
+    navbar.classList.remove('navbar-shrink');
+  }
+};
+
+window.addEventListener('scroll', updateNavbarState, { passive: true });
+updateNavbarState();
+
 // Close mobile menu on link click (for persistent navigation)
 swup.hooks.on('link:click', () => {
   const navbarCollapse = document.querySelector('.navbar-collapse');
@@ -31,4 +46,6 @@ swup.hooks.on('page:view', () => {
   if (typeof AOS !== 'undefined') {
     AOS.refresh();
   }
+
+  updateNavbarState();
 });
